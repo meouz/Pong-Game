34ed213c5ca81b5ce9db72f8cd602b3562deeedc
Retouchable <109464156+Retouchable@users.noreply.github.com>
2023-09-15 20:43:02 +0700

    Init
    Setup Project

---
+++ My project/Assets/Test/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int speed;
+    public KeyCode upKey;
+    public KeyCode downKey;
+    private Rigidbody2D rig;
+
+    // Start is called before the first frame update
+    private void Start() {
+        rig = GetComponent<Rigidbody2D>();    
+    }
+
+    // Update is called once per frame
+    private void Update() {
+        ////return ke zero ketika tidak ada input
+        ////Vector2 movement = Vector3.zero;
+
+        MoveObject(GetInput());
+
+        //get input
+        //Vector3 movement = GetInput();
+        //move object
+        //MoveObject(movement);
+    }
+
+    private Vector2 GetInput() {
+        if (Input.GetKey(upKey)) {
+            //atas
+            //movement = Vector3.right * speed;
+            return Vector2.up * speed; //seharusnya Vector2.up tapi kenapa outputnya ke kiri?
+        }
+        else if (Input.GetKey(downKey)) {
+            //bawah
+            //movement = Vector3.left * speed;
+            return Vector2.down * speed;
+        }
+        return Vector2.zero;
+    }
+    private void MoveObject(Vector2 movement) {
+        //transform.Translate(movement * Time.deltaTime); karena sekarang sudah menggunakan velocity
+        rig.velocity = movement;
+    }
+}
